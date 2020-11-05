213453afc8b5be8d56d0702f0d095cf846e2e929
theo <theo.lambert@viacesi.fr>
2020-11-05 21:29:05 +0100

    fix(styles): fixed mobile styles
    - updated styles.scss, about.scss, contact.scss and article.scss
    - added meta tag with viewport
    - updated version to 1.2

--- mastasigen.py
+++ mastasigen.py
@@ -15,7 +15,7 @@ from page import Page
 
 class Mastasigen:
     def __init__(self):
-        self.version = '1.2-beta'
+        self.version = '1.2'
         self.parser = argparse.ArgumentParser(
             prog='Mastasigen!',
             usage='static website generation',
